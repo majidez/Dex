b75c38f6e3a43915c2ef28304f2b5669c7fb48c6
majido6969 <abdisalat999@gmail.com>
2026-09-25 13:58:43 +0100

    Lesson 7 - Built FetchService and getPokemon function; pokemon fetching from online successfully.

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
     
+    let fetcher = FetchService()
+    
     var body: some View {
         NavigationView {
             List {
@@ -33,11 +35,38 @@ struct ContentView: View {
                 }
                 ToolbarItem {
                     Button("Add Item", systemImage: "plus") {
-                        
+                        getPokemon()
                     }
                 }
             }
             Text("Select an item")
+        }
+    }
+    
+    private func getPokemon() {
+        Task {
+            for id in 1..<152 {
+                do {
+                    let fetchedPokemon = try await fetcher.fetchPokemon(id)
+                    
+                    let pokemon = Pokemon(context: viewContext)
+                    pokemon.id = fetchedPokemon.id
+                    pokemon.name = fetchedPokemon.name
+                    pokemon.types = fetchedPokemon.types
+                    pokemon.hp = fetchedPokemon.hp
+                    pokemon.attack = fetchedPokemon.attack
+                    pokemon.defense = fetchedPokemon.defense
+                    pokemon.specialAttack = fetchedPokemon.specialAttack
+                    pokemon.specialDefense = fetchedPokemon.specialDefense
+                    pokemon.speed = fetchedPokemon.speed
+                    pokemon.sprite = fetchedPokemon.sprite
+                    pokemon.shiny = fetchedPokemon.shiny
+                    
+                    try viewContext.save()
+                } catch {
+                    print(error)
+                }
+            }
         }
     }
     
---
+++ Dex/FetchService.swift
@@ -0,0 +1,34 @@
+//
+//  FetchService.swift
+//  Dex
+//
+//  Created by Abdimajid Ibrahim on 25/09/2026.
+//
+
+import Foundation
+
+struct FetchService {
+    enum FetchError: Error {
+        case badResponse
+    }
+    
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
+    func fetchPokemon(_ id: Int) async throws -> FetchedPokemon {
+        let fetchURL = baseURL.appending(path: String(id))
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let pokemon = try decoder.decode(FetchedPokemon.self, from: data)
+        print("FetchedPokemon: \(pokemon.id): \(pokemon.name)")
+        
+        return pokemon
+    }
+}
